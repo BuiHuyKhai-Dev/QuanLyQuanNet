6c8e3fb835a5b87b196209ccf14cd5800c97ee11
nnhiyen@ <164819900+nnhiyen@users.noreply.github.com>
2025-04-22 14:24:26 +0700

    Update NhanVienDTO.java

--- src/DTO/NhanVienDTO.java
+++ src/DTO/NhanVienDTO.java
@@ -1,106 +1,69 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package DTO;
 
-/**
- *
- * @author Minnie
- */
 public class NhanVienDTO {
-    private int maNV;
-    private String hoNV;
+    private String maNV;
     private String tenNV;
+    private String matKhau;
+    private String cccd;
+    private String soDT;
+    private String ngaySinh;
+    private String ngayDangKy;
     private String username;
-    private String matkhau;
-    private int role;
-    
-    public NhanVienDTO(){}
+    private String role;
+    private String chucVu;
+    private int trangThai;
 
-    public NhanVienDTO(int maNV, String hoNV, String tenNV, String username, String matkhau, int role) {
+    // Constructor mặc định
+    public NhanVienDTO() {}
+
+    // Constructor đầy đủ tham số
+    public NhanVienDTO(String maNV, String tenNV, String matKhau, String cccd, 
+                      String soDT, String ngaySinh, String ngayDangKy, 
+                      String username, String role, String chucVu, int trangThai) {
         this.maNV = maNV;
-        this.hoNV = hoNV;
         this.tenNV = tenNV;
+        this.matKhau = matKhau;
+        this.cccd = cccd;
+        this.soDT = soDT;
+        this.ngaySinh = ngaySinh;
+        this.ngayDangKy = ngayDangKy;
         this.username = username;
-        this.matkhau = matkhau;
         this.role = role;
+        this.chucVu = chucVu;
+        this.trangThai = trangThai;
     }
 
-    public int getMaNV() {
-        return maNV;
-    }
+    // Getter & Setter
+    public String getMaNV() { return maNV; }
+    public void setMaNV(String maNV) { this.maNV = maNV; }
 
-    public void setMaNV(int maNV) {
-        this.maNV = maNV;
-    }
+    public String getTenNV() { return tenNV; }
+    public void setTenNV(String tenNV) { this.tenNV = tenNV; }
 
-    public String getHoNV() {
-        return hoNV;
-    }
+    public String getMatKhau() { return matKhau; }
+    public void setMatKhau(String matKhau) { this.matKhau = matKhau; }
 
-    public void setHoNV(String hoNV) {
-        this.hoNV = hoNV;
-    }
+    public String getCccd() { return cccd; }
+    public void setCccd(String cccd) { this.cccd = cccd; }
 
-    public String getTenNV() {
-        return tenNV;
-    }
+    public String getSoDT() { return soDT; }
+    public void setSoDT(String soDT) { this.soDT = soDT; }
 
-    public void setTenNV(String tenNV) {
-        this.tenNV = tenNV;
-    }
+    public String getNgaySinh() { return ngaySinh; }
+    public void setNgaySinh(String ngaySinh) { this.ngaySinh = ngaySinh; }
 
-    public String getUsername() {
-        return username;
-    }
+    public String getNgayDangKy() { return ngayDangKy; }
+    public void setNgayDangKy(String ngayDangKy) { this.ngayDangKy = ngayDangKy; }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
 
-    public String getMatkhau() {
-        return matkhau;
-    }
+    public String getRole() { return role; }
+    public void setRole(String role) { this.role = role; }
 
-    public void setMatkhau(String matkhau) {
-        this.matkhau = matkhau;
-    }
+    public String getChucVu() { return chucVu; }
+    public void setChucVu(String chucVu) { this.chucVu = chucVu; }
 
-    public int getRole() {
-        return role;
-    }
-
-    public void setRole(int role) {
-        this.role = role;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 3;
-        hash = 31 * hash + this.maNV;
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final NhanVienDTO other = (NhanVienDTO) obj;
-        return this.maNV == other.maNV;
-    }
-
-    @Override
-    public String toString() {
-        return "NhanVienDTO{" + "maNV=" + maNV + ", hoNV=" + hoNV + ", tenNV=" + tenNV + ", username=" + username + ", matkhau=" + matkhau + ", role=" + role + '}';
-    }
-    
-    
+    public int getTrangThai() { return trangThai; }
+    public void setTrangThai(int trangThai) { this.trangThai = trangThai; }
 }
