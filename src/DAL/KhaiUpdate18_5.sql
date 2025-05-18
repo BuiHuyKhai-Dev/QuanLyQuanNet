1abcb556e05e491a871212cdd23ae427a4e3a109
Nhan <thanhnhan140705@gmail.com>
2025-05-18 20:18:17 +0700

    database

--- src/DAL/KhaiUpdate18_5.sql
+++ src/DAL/KhaiUpdate18_5.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 13, 2025 at 08:26 PM
+-- Generation Time: May 18, 2025 at 03:17 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -80,23 +80,70 @@ INSERT INTO `chitiethoadon` (`MaHD`, `MaHang`, `SoLuong`, `DonGia`, `ThanhTien`,
 
 CREATE TABLE `chitietphieunhap` (
   `MaPN` int(11) NOT NULL,
-  `MaHang` int(11) NOT NULL,
+  `MaThucAn` int(11) NOT NULL,
   `SoLuong` int(11) DEFAULT NULL,
   `DonGia` decimal(12,2) DEFAULT NULL,
-  `ThanhTien` decimal(12,2) DEFAULT NULL,
-  `created_at` datetime DEFAULT current_timestamp()
+  `ThanhTien` decimal(12,2) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `chitietphieunhap`
 --
 
-INSERT INTO `chitietphieunhap` (`MaPN`, `MaHang`, `SoLuong`, `DonGia`, `ThanhTien`, `created_at`) VALUES
-(1, 1, 10, 200000.00, 2000000.00, '2025-04-18 21:30:42'),
-(2, 2, 15, 300000.00, 4500000.00, '2025-04-18 21:30:42'),
-(3, 3, 20, 350000.00, 7000000.00, '2025-04-18 21:30:42'),
-(4, 4, 12, 400000.00, 4800000.00, '2025-04-18 21:30:42'),
-(5, 5, 18, 500000.00, 9000000.00, '2025-04-18 21:30:42');
+INSERT INTO `chitietphieunhap` (`MaPN`, `MaThucAn`, `SoLuong`, `DonGia`, `ThanhTien`) VALUES
+(1, 1, 10, 200000.00, 2000000.00),
+(2, 2, 15, 300000.00, 4500000.00),
+(3, 3, 20, 350000.00, 7000000.00),
+(4, 4, 12, 400000.00, 4800000.00),
+(5, 5, 18, 500000.00, 9000000.00),
+(7, 4, 20, 20000.00, 400000.00),
+(7, 5, 30, 35000.00, 1050000.00),
+(8, 1, 50, 25000.00, 1250000.00),
+(8, 4, 30, 20000.00, 600000.00),
+(9, 1, 50, 25000.00, 1250000.00),
+(9, 5, 70, 35000.00, 2450000.00),
+(9, 6, 50, 35000.00, 1750000.00),
+(10, 1, 50, 25000.00, 1250000.00),
+(10, 5, 30, 35000.00, 1050000.00),
+(11, 1, 50, 25000.00, 1250000.00),
+(11, 4, 70, 20000.00, 1400000.00),
+(11, 5, 70, 35000.00, 2450000.00),
+(12, 3, 50, 70000.00, 3500000.00),
+(12, 6, 50, 35000.00, 1750000.00),
+(13, 1, 20, 25000.00, 500000.00),
+(13, 2, 20, 15000.00, 300000.00),
+(14, 1, 30, 25000.00, 750000.00),
+(14, 2, 30, 15000.00, 450000.00),
+(15, 1, 50, 25000.00, 1250000.00),
+(15, 2, 50, 15000.00, 750000.00),
+(16, 4, 100, 20000.00, 2000000.00),
+(16, 5, 100, 35000.00, 3500000.00),
+(16, 6, 100, 35000.00, 3500000.00);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `danhmucchucnang`
+--
+
+CREATE TABLE `danhmucchucnang` (
+  `machucnang` int(11) NOT NULL,
+  `tenchucnang` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `danhmucchucnang`
+--
+
+INSERT INTO `danhmucchucnang` (`machucnang`, `tenchucnang`) VALUES
+(1, 'Quản lý máy'),
+(2, 'Quản lý khách hàng'),
+(3, 'Quản lý nhân viên'),
+(4, 'Quản lý nhà cung cấp'),
+(5, 'Quản lý nhập hàng'),
+(6, 'Quản lý kho'),
+(7, 'Thống kê'),
+(8, 'Phân quyền');
 
 -- --------------------------------------------------------
 
@@ -125,7 +172,9 @@ INSERT INTO `donhangthucan` (`MaDH`, `MaKH`, `MaMay`, `NgayDat`, `TongTien`, `Tr
 (3, 3, 3, '2025-04-12 00:00:00', 120000.00, 3, '2025-04-18 21:20:01', 5),
 (4, 4, 4, '2025-04-13 00:00:00', 65000.00, 4, '2025-04-18 21:20:01', 3),
 (5, 5, 5, '2025-04-14 00:00:00', 90000.00, 1, '2025-04-18 21:20:01', 2),
-(6, 3, 2, '2025-05-13 16:32:35', 60000.00, 1, '2025-05-13 16:32:35', 1);
+(6, 3, 2, '2025-05-13 16:32:35', 60000.00, 1, '2025-05-13 16:32:35', 1),
+(7, 1, 1, '2025-05-14 16:09:54', 85000.00, 1, '2025-05-14 16:09:54', 1),
+(8, 1, 1, '2025-05-14 16:09:58', 0.00, 1, '2025-05-14 16:09:58', 1);
 
 -- --------------------------------------------------------
 
@@ -177,7 +226,7 @@ CREATE TABLE `khachhang` (
 
 INSERT INTO `khachhang` (`MaKH`, `TenKH`, `SoDienThoai`, `Email`, `SoDuTaiKhoan`, `created_at`) VALUES
 (1, 'Nguyen Thi Mai', '0987654321', 'nguyenmai@gmail.com', -238795.79, '2025-04-18 21:20:01'),
-(3, 'Le Thi Lan', '0978567345', 'lelan@hotmail.com', 999982.93, '2025-04-18 21:20:01'),
+(3, 'Le Thi Lan', '0978567345', 'lelan@hotmail.com', 977088.75, '2025-04-18 21:20:01'),
 (4, 'Pham Quang Hieu', '0945671234', 'phamquanghieu@outlook.com', -1941504.63, '2025-04-18 21:20:01'),
 (5, 'Nguyen Bao Hoa', '0906789123', 'nguyenhoa@gmail.com', 200000.00, '2025-04-18 21:20:01'),
 (6, 'Bùi Huy Khải', '0796137759', 'ok@gmail.com', 100000.00, '2025-05-10 17:14:59'),
@@ -223,6 +272,7 @@ INSERT INTO `kho` (`MaKho`, `TenKho`, `DiaChi`, `SucChua`, `created_at`) VALUES
 
 CREATE TABLE `khomaytinh` (
   `MaKho` int(11) NOT NULL,
+  `MaNCC` int(10) NOT NULL,
   `MaMay` int(11) NOT NULL,
   `SoLuong` int(11) DEFAULT NULL,
   `created_at` datetime DEFAULT current_timestamp()
@@ -232,12 +282,12 @@ CREATE TABLE `khomaytinh` (
 -- Dumping data for table `khomaytinh`
 --
 
-INSERT INTO `khomaytinh` (`MaKho`, `MaMay`, `SoLuong`, `created_at`) VALUES
-(1, 1, 10, '2025-04-18 21:20:01'),
-(2, 2, 5, '2025-04-18 21:20:01'),
-(3, 3, 8, '2025-04-18 21:20:01'),
-(4, 4, 12, '2025-04-18 21:20:01'),
-(5, 5, 15, '2025-04-18 21:20:01');
+INSERT INTO `khomaytinh` (`MaKho`, `MaNCC`, `MaMay`, `SoLuong`, `created_at`) VALUES
+(1, 1, 1, 1, '2025-04-18 21:20:01'),
+(2, 1, 2, 1, '2025-04-18 21:20:01'),
+(3, 1, 3, 1, '2025-04-18 21:20:01'),
+(4, 2, 4, 1, '2025-04-18 21:20:01'),
+(5, 2, 5, 1, '2025-04-18 21:20:01');
 
 -- --------------------------------------------------------
 
@@ -247,6 +297,7 @@ INSERT INTO `khomaytinh` (`MaKho`, `MaMay`, `SoLuong`, `created_at`) VALUES
 
 CREATE TABLE `khothucan` (
   `MaKho` int(11) NOT NULL,
+  `MaNCC` int(11) NOT NULL,
   `MaThucAn` int(11) NOT NULL,
   `SoLuong` int(11) DEFAULT NULL,
   `created_at` datetime DEFAULT current_timestamp()
@@ -256,12 +307,12 @@ CREATE TABLE `khothucan` (
 -- Dumping data for table `khothucan`
 --
 
-INSERT INTO `khothucan` (`MaKho`, `MaThucAn`, `SoLuong`, `created_at`) VALUES
-(1, 1, 50, '2025-04-18 21:22:02'),
-(1, 2, 100, '2025-04-18 21:22:02'),
-(2, 3, 150, '2025-04-18 21:22:02'),
-(3, 4, 80, '2025-04-18 21:22:02'),
-(4, 5, 120, '2025-04-18 21:22:02');
+INSERT INTO `khothucan` (`MaKho`, `MaNCC`, `MaThucAn`, `SoLuong`, `created_at`) VALUES
+(1, 3, 1, 50, '2025-04-18 21:22:02'),
+(1, 3, 2, 100, '2025-04-18 21:22:02'),
+(2, 3, 3, 150, '2025-04-18 21:22:02'),
+(3, 3, 4, 80, '2025-04-18 21:22:02'),
+(4, 3, 5, 120, '2025-04-18 21:22:02');
 
 -- --------------------------------------------------------
 
@@ -283,7 +334,7 @@ CREATE TABLE `maytinh` (
 --
 
 INSERT INTO `maytinh` (`MaMay`, `TenMay`, `TrangThai`, `ViTri`, `GiaThue`, `created_at`) VALUES
-(1, 'Máy tính A1', 1, 'Khu 1', 5000, '2025-04-18 21:20:01'),
+(1, 'Máy tính A1', 3, 'Khu 1', 5000, '2025-04-18 21:20:01'),
 (2, 'Máy tính B2', 1, 'Khu 2', 4000, '2025-04-18 21:20:01'),
 (3, 'Máy tính C3', 3, 'Khu 3', 6000, '2025-04-18 21:20:01'),
 (4, 'Máy tính D4', 1, 'Khu 4', 5500, '2025-04-18 21:20:01'),
@@ -309,9 +360,9 @@ CREATE TABLE `nhacungcap` (
 --
 
 INSERT INTO `nhacungcap` (`MaNCC`, `TenNCC`, `DiaChi`, `SoDienThoai`, `Email`, `created_at`) VALUES
-(1, 'Công ty A', 'Hà Nội', '0101010101', 'ncca@gmail.com', '2025-04-18 21:22:02'),
-(2, 'Công ty B', 'TP Hồ Chí Minh', '0202020202', 'nccb@gmail.com', '2025-04-18 21:22:02'),
-(3, 'Công ty C', 'Đà Nẵng', '0303030303', 'nccc@gmail.com', '2025-04-18 21:22:02'),
+(1, 'Công ty Phong Vũ', 'Hà Nội', '0101010101', 'ncca@gmail.com', '2025-04-18 21:22:02'),
+(2, 'Công ty GearVN', 'TP Hồ Chí Minh', '0202020202', 'nccb@gmail.com', '2025-04-18 21:22:02'),
+(3, 'Bách Hóa Xanh', 'Đà Nẵng', '0303030303', 'nccc@gmail.com', '2025-04-18 21:22:02'),
 (4, 'Công ty D', 'Cần Thơ', '0404040404', 'nccd@gmail.com', '2025-04-18 21:22:02'),
 (5, 'Công ty E', 'Bình Dương', '0505050505', 'ncce@gmail.com', '2025-04-18 21:22:02'),
 (6, 'Công ty F', 'Khánh Hòa', '0123456789', 'nccf@gmail.com', '2025-05-14 00:20:07');
@@ -348,7 +399,27 @@ INSERT INTO `nhanvien` (`MaNV`, `HoTen`, `GioiTinh`, `NgaySinh`, `SoDienThoai`, 
 (7, 'Trần Thị B', 2, '1985-05-12', '0987654321', 'b@gmail.com', 'TP Hồ Chí Minh', 12000000.00, '2025-04-18 21:22:02'),
 (8, 'Lê Văn C', 1, '1980-07-21', '0932233445', 'c@gmail.com', 'Đà Nẵng', 11000000.00, '2025-04-18 21:22:02'),
 (9, 'Phạm Thị D', 2, '1992-11-30', '0911223344', 'd@gmail.com', 'Cần Thơ', 9500000.00, '2025-04-18 21:22:02'),
-(10, 'Hoàng Văn E', 1, '1995-09-15', '0900112233', 'e@gmail.com', 'Bình Dương', 11500000.00, '2025-04-18 21:22:02');
+(10, 'Hoàng Văn E', 1, '1995-09-15', '0900112233', 'e@gmail.com', 'Bình Dương', 11500000.00, '2025-04-18 21:22:02'),
+(11, 'Nguyễn Văn Z', 1, '2001-07-13', '0123456789', 'z@gmail.com', 'Thanh Hóa', 1200000.00, '2025-05-14 13:19:33');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `nhomquyen`
+--
+
+CREATE TABLE `nhomquyen` (
+  `idnhomquyen` int(11) NOT NULL,
+  `tennhomquyen` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `nhomquyen`
+--
+
+INSERT INTO `nhomquyen` (`idnhomquyen`, `tennhomquyen`) VALUES
+(1, 'Quản lý'),
+(2, 'Nhân viên');
 
 -- --------------------------------------------------------
 
@@ -359,10 +430,8 @@ INSERT INTO `nhanvien` (`MaNV`, `HoTen`, `GioiTinh`, `NgaySinh`, `SoDienThoai`, 
 CREATE TABLE `phieunhap` (
   `MaPN` int(11) NOT NULL,
   `MaNCC` int(11) NOT NULL,
-  `NgayNhap` datetime DEFAULT NULL,
-  `NhanVienNhap` varchar(100) DEFAULT NULL,
+  `MaNV` int(100) DEFAULT NULL,
   `TongTien` decimal(12,2) DEFAULT NULL,
-  `LoaiHang` int(11) DEFAULT NULL,
   `created_at` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -370,12 +439,23 @@ CREATE TABLE `phieunhap` (
 -- Dumping data for table `phieunhap`
 --
 
-INSERT INTO `phieunhap` (`MaPN`, `MaNCC`, `NgayNhap`, `NhanVienNhap`, `TongTien`, `LoaiHang`, `created_at`) VALUES
-(1, 1, '2025-04-01 00:00:00', '1', 5000000.00, 1, '2025-04-18 21:22:02'),
-(2, 2, '2025-04-02 00:00:00', '2', 6000000.00, 1, '2025-04-18 21:22:02'),
-(3, 3, '2025-04-03 00:00:00', '3', 7000000.00, 2, '2025-04-18 21:22:02'),
-(4, 4, '2025-04-04 00:00:00', '4', 5500000.00, 2, '2025-04-18 21:22:02'),
-(5, 5, '2025-04-05 00:00:00', '5', 8000000.00, 1, '2025-04-18 21:22:02');
+INSERT INTO `phieunhap` (`MaPN`, `MaNCC`, `MaNV`, `TongTien`, `created_at`) VALUES
+(1, 1, 1, 2000000.00, '2025-04-18 21:22:02'),
+(2, 2, 2, 4500000.00, '2025-04-18 21:22:02'),
+(3, 3, 3, 7000000.00, '2025-04-18 21:22:02'),
+(4, 4, 4, 4800000.00, '2025-04-18 21:22:02'),
+(5, 5, 5, 9000000.00, '2025-04-18 21:22:02'),
+(6, 1, 1, 3000000.00, '2025-05-16 23:34:12'),
+(7, 3, 2, 1450000.00, '2025-05-16 23:37:40'),
+(8, 3, 1, 1850000.00, '2025-05-16 23:42:08'),
+(9, 3, 5, 5450000.00, '2025-05-16 23:47:04'),
+(10, 1, 1, 2300000.00, '2025-05-16 23:55:10'),
+(11, 1, 1, 5100000.00, '2025-05-16 23:58:26'),
+(12, 3, 1, 5250000.00, '2025-05-17 00:01:37'),
+(13, 3, 2, 800000.00, '2025-05-17 00:07:01'),
+(14, 1, 1, 1200000.00, '2025-05-17 00:12:53'),
+(15, 1, 1, 2000000.00, '2025-05-17 00:16:16'),
+(16, 3, 1, 9000000.00, '2025-05-17 00:19:16');
 
 -- --------------------------------------------------------
 
@@ -404,7 +484,7 @@ INSERT INTO `sudungmay` (`MaSuDung`, `MaNV`, `MaKH`, `MaMay`, `ThoiGianBatDau`, 
 (49, 1, 5, 5, '2025-04-19 21:45:48', '2025-04-19 22:05:48', 0.33, 15000.00, '2025-04-19 21:45:48', 1),
 (50, 1, 2, 2, '2025-04-19 21:46:20', '2025-04-19 21:52:44', 0.11, 4266.67, '2025-04-19 21:46:20', 0),
 (51, 1, 2, 2, '2025-04-19 21:52:50', '2025-04-19 21:53:17', 0.01, 300.00, '2025-04-19 21:52:50', 0),
-(53, 1, 4, 2, '2025-04-19 21:53:26', '2025-04-19 22:05:44', 0.21, 8200.00, '2025-04-19 21:53:26', 1),
+(53, 1, 4, 2, '2025-04-19 21:53:26', '2025-04-19 22:05:44', 0.21, 8200.00, '2025-04-19 21:53:26', 0),
 (54, 1, 1, 1, '2025-04-19 21:55:59', '2025-04-19 21:56:01', 0.00, 27.78, '2025-04-19 21:55:59', 0),
 (55, 1, 1, 1, '2025-05-11 14:12:26', '2025-05-11 14:12:27', 0.00, 13.89, '2025-05-11 14:12:26', 0),
 (56, 1, 1, 1, '2025-05-11 14:20:50', '2025-05-11 14:20:51', 0.00, 13.89, '2025-05-11 14:20:50', 0),
@@ -413,7 +493,8 @@ INSERT INTO `sudungmay` (`MaSuDung`, `MaNV`, `MaKH`, `MaMay`, `ThoiGianBatDau`, 
 (59, 1, 1, 1, '2025-05-13 16:18:57', '2025-05-13 16:18:58', 0.00, 1.39, '2025-05-13 16:18:57', 0),
 (60, 1, 1, 1, '2025-05-13 16:19:07', '2025-05-13 16:19:08', 0.00, 1.39, '2025-05-13 16:19:07', 0),
 (61, 1, 4, 1, '2025-05-13 16:19:18', '2025-05-13 16:19:19', 0.00, 1.39, '2025-05-13 16:19:18', 0),
-(62, 1, 3, 1, '2025-05-13 16:20:57', '2025-05-13 16:21:09', 0.00, 16.67, '2025-05-13 16:20:57', 1);
+(62, 1, 3, 1, '2025-05-13 16:20:57', '2025-05-13 16:21:09', 0.00, 16.67, '2025-05-13 16:20:57', 1),
+(63, 1, 3, 2, '2025-05-14 16:04:29', '2025-05-14 21:47:53', 5.72, 22893.33, '2025-05-14 16:04:29', 1);
 
 -- --------------------------------------------------------
 
@@ -448,9 +529,8 @@ INSERT INTO `taikhoan` (`tendangnhap`, `matkhau`, `manhomquyen`, `trangthai`) VA
 CREATE TABLE `thucan` (
   `MaThucAn` int(11) NOT NULL,
   `TenThucAn` varchar(255) DEFAULT NULL,
-  `DonVi` varchar(50) DEFAULT NULL,
+  `SoLuong` int(11) NOT NULL,
   `DonGia` decimal(12,2) DEFAULT NULL,
-  `HanSuDung` date DEFAULT NULL,
   `created_at` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -458,12 +538,14 @@ CREATE TABLE `thucan` (
 -- Dumping data for table `thucan`
 --
 
-INSERT INTO `thucan` (`MaThucAn`, `TenThucAn`, `DonVi`, `DonGia`, `HanSuDung`, `created_at`) VALUES
-(1, 'Cơm chiên', 'Phần', 25000.00, '2025-12-31', '2025-04-18 21:20:01'),
-(2, 'Bánh mì', 'Cái', 15000.00, '2025-06-30', '2025-04-18 21:20:01'),
-(3, 'Pizza', 'Cái', 70000.00, '2025-08-15', '2025-04-18 21:20:01'),
-(4, 'Mì xào', 'Phần', 20000.00, '2025-11-20', '2025-04-18 21:20:01'),
-(5, 'Gà rán', 'Cái', 35000.00, '2025-07-10', '2025-04-18 21:20:01');
+INSERT INTO `thucan` (`MaThucAn`, `TenThucAn`, `SoLuong`, `DonGia`, `created_at`) VALUES
+(1, 'Cơm chiên', 200, 25000.00, '2025-04-18 21:20:01'),
+(2, 'Bánh mì', 200, 15000.00, '2025-04-18 21:20:01'),
+(3, 'Pizza', 200, 70000.00, '2025-04-18 21:20:01'),
+(4, 'Mì xào', 200, 20000.00, '2025-04-18 21:20:01'),
+(5, 'Gà rán', 200, 35000.00, '2025-04-18 21:20:01'),
+(6, 'Lòng xào dưa', 200, 35000.00, '2025-05-16 18:57:13'),
+(7, 'Gà xào xả ớt', 0, 45000.00, '2025-05-17 00:30:05');
 
 --
 -- Indexes for dumped tables
@@ -486,7 +568,13 @@ ALTER TABLE `chitiethoadon`
 -- Indexes for table `chitietphieunhap`
 --
 ALTER TABLE `chitietphieunhap`
-  ADD PRIMARY KEY (`MaPN`,`MaHang`);
+  ADD PRIMARY KEY (`MaPN`,`MaThucAn`);
+
+--
+-- Indexes for table `danhmucchucnang`
+--
+ALTER TABLE `danhmucchucnang`
+  ADD PRIMARY KEY (`machucnang`);
 
 --
 -- Indexes for table `donhangthucan`
@@ -548,6 +636,12 @@ ALTER TABLE `nhanvien`
   ADD PRIMARY KEY (`MaNV`);
 
 --
+-- Indexes for table `nhomquyen`
+--
+ALTER TABLE `nhomquyen`
+  ADD PRIMARY KEY (`idnhomquyen`);
+
+--
 -- Indexes for table `phieunhap`
 --
 ALTER TABLE `phieunhap`
@@ -577,7 +671,7 @@ ALTER TABLE `thucan`
 -- AUTO_INCREMENT for table `donhangthucan`
 --
 ALTER TABLE `donhangthucan`
-  MODIFY `MaDH` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `MaDH` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `hoadon`
@@ -613,25 +707,25 @@ ALTER TABLE `nhacungcap`
 -- AUTO_INCREMENT for table `nhanvien`
 --
 ALTER TABLE `nhanvien`
-  MODIFY `MaNV` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `MaNV` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `phieunhap`
 --
 ALTER TABLE `phieunhap`
-  MODIFY `MaPN` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `MaPN` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `sudungmay`
 --
 ALTER TABLE `sudungmay`
-  MODIFY `MaSuDung` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=63;
+  MODIFY `MaSuDung` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=64;
 
 --
 -- AUTO_INCREMENT for table `thucan`
 --
 ALTER TABLE `thucan`
-  MODIFY `MaThucAn` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `MaThucAn` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- Constraints for dumped tables
@@ -645,12 +739,6 @@ ALTER TABLE `chitietdonthucan`
   ADD CONSTRAINT `chitietdonthucan_ibfk_2` FOREIGN KEY (`MaThucAn`) REFERENCES `thucan` (`MaThucAn`);
 
 --
--- Constraints for table `chitietphieunhap`
---
-ALTER TABLE `chitietphieunhap`
-  ADD CONSTRAINT `chitietphieunhap_ibfk_1` FOREIGN KEY (`MaPN`) REFERENCES `phieunhap` (`MaPN`);
-
---
 -- Constraints for table `donhangthucan`
 --
 ALTER TABLE `donhangthucan`
@@ -661,12 +749,6 @@ ALTER TABLE `donhangthucan`
 --
 ALTER TABLE `khomaytinh`
   ADD CONSTRAINT `khomaytinh_ibfk_2` FOREIGN KEY (`MaMay`) REFERENCES `maytinh` (`MaMay`);
-
---
--- Constraints for table `khothucan`
---
-ALTER TABLE `khothucan`
-  ADD CONSTRAINT `khothucan_ibfk_2` FOREIGN KEY (`MaThucAn`) REFERENCES `thucan` (`MaThucAn`);
 
 --
 -- Constraints for table `sudungmay`
